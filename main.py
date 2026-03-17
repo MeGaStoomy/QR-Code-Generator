e53c3f235576ce7ef89b4ecb5114f46f7220462c
MeGaStoomy <lorenzo.famillesd@gmail.com>
2026-03-17 19:54:37 +0100

    Switched from Tkinter to PyQt6 for more freedom and better performance. There is only basic initialization for now.

--- main.py
+++ main.py
@@ -1,26 +1,66 @@
-import os, random
-from tkinter import *
+"""
+Done using no pre-made modules for qr code creation such as qrcode, pyqrcode, or other.
+Any and all classes were coded by myself, not copied from the internet.
+This was made as a proof of skill and knowledge.
 
-class QRCode:
+Began on March 10th 2026
+"""
+
+import os
+import random
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QPushButton,
+    QLineEdit,
+    QWidget,
+)
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPalette, QColor
+
+class Program:
     def __init__(self):
-        main = Tk()
-        self.main = main
-        main.title("QR Code Generator")
-        main.configure(bg="#5a0088")
-        SCREENW = main.winfo_screenwidth()-10
-        SCREENH = main.winfo_screenheight()-10
-        HEIGHT = int(SCREENH*0.8)
-        WIDTH = int(SCREENW*0.6)
+        self.app = Application()
+        self.window = Window()
+
+class Application(QApplication):
+    def __init__(self):
+        super().__init__([])
+
+class Window(QWidget):
+    def __init__(self):
+        '''Initializes the UI for the application.'''
+        super().__init__()
+        self._setupWindow()
+        self._setBackgroundColor()
+        
+    def _setupWindow(self):
+        '''Creates and sets up the window geometry'''
+        SCREENW,SCREENH,HEIGHT,WIDTH,X,Y = self._calculateWindowGeometry()
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.setGeometry(X,Y,WIDTH,HEIGHT)
+
+    def _setBackgroundColor(self):
+        color = QColor(98, 32, 159)
+        self.palette().setColor(QPalette.ColorRole.Window,color)
+        
+        
+    def _calculateWindowGeometry(self):
+        '''Creates some constants used for UI creation'''
+        availableSpace = self.screen().availableGeometry()
+        SCREENW = availableSpace.width()
+        SCREENH = availableSpace.height()
+        HEIGHT = int(SCREENH*0.9)
+        WIDTH = int(SCREENW*0.7)
         X = SCREENW//2-WIDTH//2
-        Y = SCREENH//2-HEIGHT//2-40
-        main.geometry(f"{WIDTH}x{HEIGHT}+{X}+{Y}")
-        self.area = Canvas(main, bg="white", height=HEIGHT-40, width=HEIGHT-40)
-        self.area.place(x=20, y=20)
-
-qr = QRCode()
+        Y = SCREENH//2-HEIGHT//2
+        return SCREENW,SCREENH,HEIGHT,WIDTH,X,Y
 
 
-
+if __name__ == '__main__':
+    program = Program()
+    program.window.show()
+    program.app.exec()
 
 
 
